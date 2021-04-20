cc62217678803b4671a33ff6315212db06219116
Maarten Meijer <maarten0912@gmail.com>
2021-04-20 14:52:56 +0200

    Haskell yeeeeeeeeeeeeee

--- out/production/pp-code/pp/block1/fp/Exercises1.hs
+++ out/production/pp-code/pp/block1/fp/Exercises1.hs
@@ -26,11 +26,13 @@ prop_com_minus :: Int -> Int -> Bool
 prop_com_minus a b = a - b == b - a
 
 --1-FP.4
+
 code :: Char -> Char
 code a
-    | (a < 'A') || (a > 'z') || ((a > 'Z') && (a < 'a')) = a
+    | a < 'A' = a
     | a <= 'Z' = if ord a < 88 then chr (ord a + 3)  else chr (ord a - 23)
     | a <= 'z' = if ord a < 120 then chr (ord a + 3) else chr (ord a - 23)
+    | otherwise = a
 
 shift ::Int -> Char -> Char
 shift n a
@@ -43,44 +45,6 @@ prop_code a = code a == shift 3 a
 
 prop_code_inverse :: Char -> Int -> Property
 prop_code_inverse a n = (n >= 0) ==> shift (26 - n) (shift n a) == a
-
---1.FP.5
-interest :: Double -> Double -> Double -> Double
-interest a 1 r = a * (100 + r) / 100
-interest a n r = (interest a (n - 1) r) * (100 + r) / 100
-
---1.FP.6
-
-discr :: Double -> Double -> Double -> Double
-discr a b c = b * b - 4 * a * c
-
-root1 :: Double -> Double -> Double -> Double
-root1 a b c
-    | discr a b c >= 0 = (-b + sqrt (discr a b c)) / (2 * a)
-    | otherwise = error "negative discriminant"
-
-root2 :: Double -> Double -> Double -> Double
-root2 a b c
-    | discr a b c >= 0 = (-b - sqrt (discr a b c)) / (2 * a)
-    | otherwise = error "negative discriminant"
-
---1.FP.7
-g :: Double -> Double -> Double -> Double -> Double
-g a b c x = a * x * x + b * x + c
-
-extrX :: Double -> Double -> Double -> Double
-extrX a b c = -b / (2 * a)
-
-extrY :: Double -> Double -> Double -> Double
-extrY a b c = g a b c (extrX a b c)
-
---1.FP.8
-mylength :: [a] -> Int
-mylength [] = 0
-mylength (x:xs) = mylength xs + 1
-
-prop_mylength :: [a] -> Bool
-prop_mylength xs = mylength xs == length xs
 
 --QuickCheck
 return []
