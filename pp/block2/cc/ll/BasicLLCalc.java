7a9d2e538d36a740db413d1f98a1d99d31ecbacb
Pepijn <p.visser-2@student.utwente.nl>
2021-04-30 12:44:51 +0200

    'getfollow'

--- pp/block2/cc/ll/BasicLLCalc.java
+++ pp/block2/cc/ll/BasicLLCalc.java
@@ -4,10 +4,7 @@ import pp.block2.cc.NonTerm;
 import pp.block2.cc.Symbol;
 import pp.block2.cc.Term;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /** Interface for a calculator that determines for a given grammar
  * (to be passed in as an argument to the constructor) what the
@@ -37,8 +34,39 @@ public class BasicLLCalc implements LLCalc {
 
     /** Returns the FOLLOW-map for the grammar of this calculator instance. */
     public Map<NonTerm, Set<Term>> getFollow() {
+        Map<NonTerm, Set<Term>> follow = new HashMap<>();
+        for (NonTerm e : g.getNonterminals()) {
+            follow.put(e, new HashSet<>());
+        }
+        follow.get(g.getStart()).add(Symbol.EOF);
+        //Initialised
+        Map<NonTerm, Set<Term>> temp = new HashMap<>();
+        while (temp != follow) {
+            temp = follow;
+            for (Rule r : g.getRules()) {
+                Set<Term> trailer = follow.get(r.getLHS());
+                System.out.println(trailer);
+                for (int i = r.getRHS().size() - 1; i >= 1; i--) {
+                    List<Symbol> bs = r.getRHS();
+                    Symbol Bi = bs.get(i);
+                    if (Bi instanceof NonTerm) {
+                        System.out.println("yes");
+                        follow.get(Bi).addAll(trailer); // update follow using trailer
+                        if (getFirst().get(Bi).contains(Symbol.EMPTY)) {
+                            Set<Term> addition = getFirst().get(Bi);
+                            addition.remove(Symbol.EMPTY);
+                            trailer.addAll(addition);
+                        } else {
+                            trailer = getFirst().get(Bi);
+                        }
+                    } else {
+                        trailer = getFirst().get(Bi);
+                    }
+                }
+            }
+        }
 
-        return null;
+        return follow;
     };
 
     /** Returns the FIRST+-map for the grammar of this calculator instance. */
