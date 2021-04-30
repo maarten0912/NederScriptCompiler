0d1a1cd8cf64e0415913772146102360d97722d4
Maarten Meijer <maarten0912@gmail.com>
2021-04-30 12:53:11 +0200

    First calc

--- pp/block2/cc/ll/BasicLLCalc.java
+++ pp/block2/cc/ll/BasicLLCalc.java
@@ -20,16 +20,48 @@ public class BasicLLCalc implements LLCalc {
 
     /** Returns the FIRST-map for the grammar of this calculator instance. */
     public Map<Symbol, Set<Term>> getFirst() {
-        Map<Symbol, Set<Term>> res = new HashMap<>();
+        Map<Symbol, Set<Term>> first = new HashMap<>();
         for (Symbol e : g.getNonterminals()) {
-            res.put(e, new HashSet<>());
+            first.put(e, new HashSet<>());
         }
-//        for (Map.Entry<Symbol, Set<Term>> e : res.entrySet()) {
-//            System.out.println(e.getKey() + " " + e.getValue());
-//        }
+        for (Term e : g.getTerminals()) {
+            first.put(e, new HashSet<>(Collections.singletonList(e)));
+        }
 
 
-        return null;
+        Map<Symbol, Set<Term>> temp = new HashMap<>();
+        while (first != temp) {
+            temp = first;
+            for (Rule r : g.getRules()) {
+                List<Symbol> betas = r.getRHS();
+                int k = r.getRHS().size();
+
+                System.out.println(r);
+
+                Set<Term> rhs = first.get(betas.get(0));
+                rhs.remove(Symbol.EMPTY);
+
+                int i = 1;
+                while (first.get(betas.get(i - 1)).contains(Symbol.EMPTY) && i <= k - 1) {
+                    Set<Term> u = first.get(betas.get(i));
+                    u.remove(Symbol.EMPTY);
+                    rhs.addAll(u);
+                    i += 1;
+                }
+
+                if (i == k && first.get(betas.get(k - 1)).contains(Symbol.EMPTY)) {
+                    rhs.add(Symbol.EMPTY);
+                }
+
+                first.get(r.getLHS()).addAll(rhs);
+            }
+        }
+        System.out.println("====> FIRST <====");
+        for (Map.Entry<Symbol, Set<Term>> e : first.entrySet()) {
+            System.out.println(e.getKey() + " " + e.getValue());
+        }
+        System.out.println("====> FIRST <====");
+        return first;
     };
 
     /** Returns the FOLLOW-map for the grammar of this calculator instance. */
