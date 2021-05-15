1a07ed06bca24ef152a7c5f2b4d5fd8b930041c2
Maarten Meijer <maarten0912@gmail.com>
2021-05-15 15:41:30 +0200

    Trying CFGBuilder

--- pp/block4/cc/cfg/BottomUpCFGBuilder.java
+++ pp/block4/cc/cfg/BottomUpCFGBuilder.java
@@ -2,6 +2,8 @@ package pp.block4.cc.cfg;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -10,6 +12,8 @@ import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import pp.block4.cc.ErrorListener;
 import pp.block4.cc.cfg.FragmentParser.BreakStatContext;
 import pp.block4.cc.cfg.FragmentParser.ContStatContext;
@@ -18,6 +22,9 @@ import pp.block4.cc.cfg.FragmentParser.ContStatContext;
 public class BottomUpCFGBuilder extends FragmentBaseListener {
 	/** The CFG being built. */
 	private Graph graph;
+	private ParseTreeWalker walker = new ParseTreeWalker();
+	private ParseTreeProperty<Node> entryNode = new ParseTreeProperty<>();
+	private ParseTreeProperty<Node> exitNode = new ParseTreeProperty<>();
 
 	/** Builds the CFG for a program contained in a given file. */
 	public Graph build(File file) {
@@ -50,15 +57,98 @@ public class BottomUpCFGBuilder extends FragmentBaseListener {
 	/** Builds the CFG for a program given as an ANTLR parse tree. */
 	public Graph build(ParseTree tree) {
 		this.graph = new Graph();
-
-
-
-
-
-
-		// TODO Fill in
-//		throw new UnsupportedOperationException("Fill in");
+		walker.walk(this, tree);
+		System.out.println(graph.size());
+		for (Node n : graph.getNodes()) {
+			System.out.println(n);
+		}
+		System.out.println("Done");
 		return this.graph;
+	}
+
+	@Override
+	public void exitProgram(FragmentParser.ProgramContext ctx) {
+//		entryNode.put(ctx,entryNode.get(ctx.getChild(0)));
+//		exitNode.put(ctx,exitNode.get(ctx.getChild(ctx.getChildCount() - 1)));
+//		for (int i = 0; i < ctx.getChildCount() - 1; i++) {
+//			Node a = exitNode.get(ctx.getChild(i));
+//			Node b = entryNode.get(ctx.getChild(i + 1));
+//			if (a != null) {
+//				a.addEdge(b);
+//			} else {
+//				System.err.println("a was null");
+//			}
+//		}
+	}
+
+	@Override
+	public void exitDecl(FragmentParser.DeclContext ctx) {
+		Node newNode = addNode(ctx, ctx.getText());
+		entryNode.put(ctx, newNode);
+		exitNode.put(ctx, newNode);
+	}
+
+	@Override
+	public void exitAssignStat(FragmentParser.AssignStatContext ctx) {
+		Node newNode = addNode(ctx, ctx.getText());
+		entryNode.put(ctx, newNode);
+		exitNode.put(ctx, newNode);
+	}
+
+	@Override
+	public void exitIfStat(FragmentParser.IfStatContext ctx) {
+		Node ifNode = addNode(ctx, ctx.getText());
+		entryNode.put(ctx, ifNode);
+		Node fakeEndNode = addNode(ctx, "End if");
+		exitNode.put(ctx, fakeEndNode);
+
+		Node thenNode = addNode(ctx.stat(0), ctx.stat(0).getText());
+		ifNode.addEdge(thenNode);
+		thenNode.addEdge(fakeEndNode);
+		if (ctx.stat().size() > 1) {
+			Node elseNode = addNode(ctx.stat(1), ctx.stat(1).getText());
+			ifNode.addEdge(elseNode);
+			elseNode.addEdge(fakeEndNode);
+		} else {
+			ifNode.addEdge(fakeEndNode);
+		}
+	}
+
+	@Override
+	public void exitWhileStat(FragmentParser.WhileStatContext ctx) {
+		Node whileNode = addNode(ctx, ctx.getText());
+		entryNode.put(ctx, whileNode);
+		Node fakeEndNode = addNode(ctx, "End while");
+		exitNode.put(ctx, fakeEndNode);
+
+		Node statNode = addNode(ctx.stat(), ctx.stat().getText());
+		whileNode.addEdge(statNode);
+		whileNode.addEdge(fakeEndNode);
+		statNode.addEdge(whileNode);
+	}
+
+	@Override
+	public void exitPrintStat(FragmentParser.PrintStatContext ctx) {
+		Node newNode = addNode(ctx, ctx.getText());
+		entryNode.put(ctx, newNode);
+		exitNode.put(ctx, newNode);
+	}
+
+	@Override
+	public void exitBlockStat(FragmentParser.BlockStatContext ctx) {
+		List<Node> nodeList = new ArrayList<>();
+		for (int i = 0; i < ctx.stat().size() - 1; i++) {
+			nodeList.add(addNode(ctx.stat(i), ctx.stat(i).getText()));
+		}
+		for (int i = 0; i < nodeList.size() - 1; i++) {
+			Node a = nodeList.get(i);
+			Node b = nodeList.get(i + 1);
+			a.addEdge(b);
+		}
+		if (!nodeList.isEmpty()) {
+			entryNode.put(ctx, nodeList.get(0));
+			exitNode.put(ctx, nodeList.get(nodeList.size()- 1));
+		}
 	}
 
 	@Override
