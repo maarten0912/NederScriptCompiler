4d5e3625b06630f90bd48a36eca8cbd03a61b5fe
Maarten Meijer <maarten0912@gmail.com>
2021-05-17 13:57:03 +0200

    LP

---
+++ pp/block4/lp/royal_family_defs.pl
@@ -0,0 +1,22 @@
+:- ['./royal_family_facts.pl'].
+
+father(X,Y) :- husband(X,Z), mother(Z,Y).
+
+child(X,Y) :- father(Y,X).
+child(X,Y) :- mother(Y,X).
+
+grandparent(X,Y) :- child(Y,Z),child(Z,X).
+
+parent(X,Y) :- child(Y,X).
+
+brother(X,Y) :- mother(Z,X),mother(Z,Y),male(X),not(X=Y).
+sister(X,Y)  :- mother(Z,X),mother(Z,Y),female(X),not(X=Y).
+
+aunt(X,Y) :- parent(Z,Y),sister(Z,X).
+uncle(X,Y) :- parent(Z,Y),brother(Z,X).
+
+cousin(X,Y) :- aunt(Z,Y),child(X,Z).
+cousin(X,Y) :- uncle(Z,Y),child(X,Z).
+
+nephew(X,Y) :- cousin(X,Y),male(X).
+niece(X,Y)  :- cousin(X,Y),female(X).
