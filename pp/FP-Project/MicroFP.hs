08ca1e3adaf54b2d955320bfac22654dacf54cf8
Pepijn <p.visser-2@student.utwente.nl>
2021-06-06 15:10:41 +0200

    deel fib werkt

--- pp/FP-Project/MicroFP.hs
+++ pp/FP-Project/MicroFP.hs
@@ -16,36 +16,44 @@ import Test.QuickCheck.All
 data Prog
     = OneFunction Function
     | MoreFunctions Function Prog
+    deriving Show
 
 data Function
     = Fun String FunArgs Expr
+    deriving Show
 
 data FunArgs
     = NoArg
     | MoreIntArgs Integer FunArgs
     | MoreIdentArgs String FunArgs
+    deriving Show
 
 data Expr
     = SingleExpr Term
     | Add Term Expr
     | Sub Term Expr
+    deriving Show
 
 data Term
     = SingleTerm Factor
     | Mult Factor Expr
+    deriving Show
 
 data Factor
     = Constant Integer
     | Identifier String CallArgs
     | IfElse Expr Orderings Expr Expr Expr
     | Parens Expr
+    deriving Show
 
 data CallArgs
     = NoCallArg
     | OneArg Expr
     | MoreArgs Expr CallArgs
+    deriving Show
 
 data Orderings = Smaller | Equal | Greater
+                deriving Show
 
 -- 〈program〉  ::= (〈function〉)+
 -- 〈function〉 ::= identifier (identifier | integer)∗ ’:=’〈expr〉’;’
@@ -57,62 +65,54 @@ data Orderings = Smaller | Equal | Greater
 --                | ’(’〈expr〉’)’
 -- 〈ordering〉 ::= ’<’ | ’==’ | ’>’
 
--- fibonacci :: Prog
--- fibonacci = 
---     MoreFunctions (
---         Fun (
---             "fibonacci",
---             MoreIntArgs (0, NoArg),
---             SingleExpr (
---                 SingleTerm (
---                     Constant (0)
---                 )
---             )
---         )
---         MoreFunctions (
---             Fun (
---                 "fibonacci",
---                 MoreIntArgs (1, NoArg),
---                 SingleExpr (
---                     SingleTerm (
---                         Constant (1)
---                     )
---                 )
---             )
---             OneFunction (
---                 Fun (
---                     "fibonacci",
---                     MoreIdentArgs ("n", NoArg), 
---                     Add (
---                         SingleTerm (
---                             Identifier (
---                                 "fibonacci",
---                                 OneArg (
---                                     Sub (
---                                         SingleTerm (Identifier ("n", NoCallArg)),
---                                         SingleExpr (SingleTerm (Constant (1)))
---                                     ) 
---                                 )
---                             )
---                         )
---                         SingleExpr (
---                             SingleTerm (
---                                 Identifier (
---                                     "fibonacci",
---                                     OneArg (
---                                         Sub (
---                                             SingleTerm (Identifier ("n", NoCallArg)),
---                                             SingleExpr (SingleTerm (Constant (2)))
---                                         ) 
---                                     )
---                                 )
---                             )
---                         )
---                     )
---                 )
---             )
---         )
---     )
+fibonacci :: Prog
+fibonacci = 
+    OneFunction (
+        Fun "fibonacci" (MoreIntArgs 0 NoArg) (SingleExpr (SingleTerm (Constant 0))))
+    --     MoreFunctions (
+    --         Fun (
+    --             "fibonacci",
+    --             MoreIntArgs (1, NoArg),
+    --             SingleExpr (
+    --                 SingleTerm (
+    --                     Constant (1)
+    --                 )
+    --             )
+    --         )
+    --         OneFunction (
+    --             Fun (
+    --                 "fibonacci",
+    --                 MoreIdentArgs ("n", NoArg), 
+    --                 Add (
+    --                     SingleTerm (
+    --                         Identifier (
+    --                             "fibonacci",
+    --                             OneArg (
+    --                                 Sub (
+    --                                     SingleTerm (Identifier ("n", NoCallArg)),
+    --                                     SingleExpr (SingleTerm (Constant (1)))
+    --                                 ) 
+    --                             )
+    --                         )
+    --                     )
+    --                     SingleExpr (
+    --                         SingleTerm (
+    --                             Identifier (
+    --                                 "fibonacci",
+    --                                 OneArg (
+    --                                     Sub (
+    --                                         SingleTerm (Identifier ("n", NoCallArg)),
+    --                                         SingleExpr (SingleTerm (Constant (2)))
+    --                                     ) 
+    --                                 )
+    --                             )
+    --                         )
+    --                     )
+    --                 )
+    --             )
+    --         )
+    --     )
+    -- )
 
 
 
