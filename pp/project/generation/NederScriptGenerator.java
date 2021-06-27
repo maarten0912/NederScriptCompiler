6edd3def4ad1632e6351f40656fe5be8b9801987
Maarten Meijer <maarten0912@gmail.com>
2021-06-27 13:51:07 +0200

    PP

--- pp/project/generation/NederScriptGenerator.java
+++ pp/project/generation/NederScriptGenerator.java
@@ -1,13 +1,19 @@
 package pp.project.generation;
 
+import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import pp.block5.cc.ErrorListener;
+import pp.block5.cc.ParseException;
+import pp.block5.cc.antlr.BuildingLexer;
+import pp.block5.cc.antlr.BuildingParser;
 import pp.iloc.model.Label;
 import pp.iloc.model.Reg;
 import pp.project.elaboration.NederScriptBaseVisitor;
+import pp.project.elaboration.NederScriptParser;
 import pp.project.elaboration.NederScriptResult;
 
-public class NederScriptGenerator extends NederScriptBaseVisitor {
+public class NederScriptGenerator extends NederScriptBaseVisitor<NederScriptInstruction> {
 
     private NederScriptResult result;
     private NederScriptProgram prog;
@@ -25,8 +31,221 @@ public class NederScriptGenerator extends NederScriptBaseVisitor {
         this.labels = new ParseTreeProperty<>();
         this.regCount = 0;
         tree.accept(this);
-        this.makeFibonacci();
+//        this.makeFibonacci();
         return this.prog;
+    }
+
+    @Override
+    public NederScriptInstruction visitProgram(NederScriptParser.ProgramContext ctx) {
+        System.out.println("Visited program");
+        return super.visitProgram(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitFunction(NederScriptParser.FunctionContext ctx) {
+        System.out.println("Visited program");
+        return new NederScriptInstruction.Branch(1,new NederScriptTarget.Rel(2));
+
+    }
+
+    @Override
+    public NederScriptInstruction visitNormalInst(NederScriptParser.NormalInstContext ctx) {
+        return super.visitNormalInst(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitIfelseInst(NederScriptParser.IfelseInstContext ctx) {
+        return super.visitIfelseInst(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitWhileInst(NederScriptParser.WhileInstContext ctx) {
+        return super.visitWhileInst(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitForInst(NederScriptParser.ForInstContext ctx) {
+        return super.visitForInst(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitNewScopeInst(NederScriptParser.NewScopeInstContext ctx) {
+        return super.visitNewScopeInst(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitAssignStat(NederScriptParser.AssignStatContext ctx) {
+        return super.visitAssignStat(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitDeclStat(NederScriptParser.DeclStatContext ctx) {
+        return super.visitDeclStat(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitReturnStat(NederScriptParser.ReturnStatContext ctx) {
+        return super.visitReturnStat(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitPrintStat(NederScriptParser.PrintStatContext ctx) {
+        return super.visitPrintStat(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitFunctionCallStat(NederScriptParser.FunctionCallStatContext ctx) {
+        return super.visitFunctionCallStat(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitIfelse(NederScriptParser.IfelseContext ctx) {
+        return super.visitIfelse(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitWhileS(NederScriptParser.WhileSContext ctx) {
+        return super.visitWhileS(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitForNormal(NederScriptParser.ForNormalContext ctx) {
+        return super.visitForNormal(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitForIn(NederScriptParser.ForInContext ctx) {
+        return super.visitForIn(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitAssign(NederScriptParser.AssignContext ctx) {
+        return super.visitAssign(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitNonTypedDecl(NederScriptParser.NonTypedDeclContext ctx) {
+        return super.visitNonTypedDecl(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitTypedDecl(NederScriptParser.TypedDeclContext ctx) {
+        return super.visitTypedDecl(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitReturnS(NederScriptParser.ReturnSContext ctx) {
+        return super.visitReturnS(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitPrint(NederScriptParser.PrintContext ctx) {
+        return super.visitPrint(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitFunCall(NederScriptParser.FunCallContext ctx) {
+        return super.visitFunCall(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitStringPrimitive(NederScriptParser.StringPrimitiveContext ctx) {
+        return super.visitStringPrimitive(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitCharacterPrimitive(NederScriptParser.CharacterPrimitiveContext ctx) {
+        return super.visitCharacterPrimitive(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitArrayPrimitive(NederScriptParser.ArrayPrimitiveContext ctx) {
+        return super.visitArrayPrimitive(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitIntegerPrimitive(NederScriptParser.IntegerPrimitiveContext ctx) {
+        return super.visitIntegerPrimitive(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitBooleanPrimitive(NederScriptParser.BooleanPrimitiveContext ctx) {
+        return super.visitBooleanPrimitive(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitType(NederScriptParser.TypeContext ctx) {
+        return super.visitType(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitPrefixExpr(NederScriptParser.PrefixExprContext ctx) {
+        return super.visitPrefixExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitParExpr(NederScriptParser.ParExprContext ctx) {
+        return super.visitParExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitVarExpr(NederScriptParser.VarExprContext ctx) {
+        return super.visitVarExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitFunCallExpr(NederScriptParser.FunCallExprContext ctx) {
+        return super.visitFunCallExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitCompExpr(NederScriptParser.CompExprContext ctx) {
+        return super.visitCompExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitPrimitiveExpr(NederScriptParser.PrimitiveExprContext ctx) {
+        return super.visitPrimitiveExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitBoolExpr(NederScriptParser.BoolExprContext ctx) {
+        return super.visitBoolExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitMultExpr(NederScriptParser.MultExprContext ctx) {
+        return super.visitMultExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitPlusExpr(NederScriptParser.PlusExprContext ctx) {
+        return super.visitPlusExpr(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitPrefixOp(NederScriptParser.PrefixOpContext ctx) {
+        return super.visitPrefixOp(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitMultOp(NederScriptParser.MultOpContext ctx) {
+        return super.visitMultOp(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitPlusOp(NederScriptParser.PlusOpContext ctx) {
+        return super.visitPlusOp(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitBoolOp(NederScriptParser.BoolOpContext ctx) {
+        return super.visitBoolOp(ctx);
+    }
+
+    @Override
+    public NederScriptInstruction visitCompOp(NederScriptParser.CompOpContext ctx) {
+        return super.visitCompOp(ctx);
     }
 
     public void makeFibonacci() {
--- pp/project/testfile.hs
+++ pp/project/testfile.hs
@@ -6,21 +6,7 @@ NederScript is a custom language created by Maarten Meijer and Pepijn Visser
 
 prog :: [Instruction] 
 prog = [ 
-    Debug ("Beginning program!!!")
-    , ReadInstr (DirAddr (65536))
-    , Receive (6)
-    , Load (ImmValue (0)) (2)
-    , Load (ImmValue (1)) (3)
-    , Compute (Gt) (2) (6) (4)
-    , Branch (4) (Abs (13))
-    , WriteInstr (2) (DirAddr (65536))
-    , Compute (Add) (2) (3) (2)
-    , Compute (Gt) (3) (6) (4)
-    , Branch (4) (Abs (13))
-    , WriteInstr (3) (DirAddr (65536))
-    , Compute (Add) (2) (3) (3)
-    , Jump (Rel (-8))
-    , EndProg
+    
     ]
 
-main = run [prog,prog]
+main = run [prog]
